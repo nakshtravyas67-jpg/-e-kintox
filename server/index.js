de5af465cf8d58605df3ea92cd4906e380046b15
vyasnakshtra66-design <vyasnakshtra66@gmail.com>
2026-06-14 21:49:03 -0700

    Fix CSP to allow Unsplash images

--- server/index.js
+++ server/index.js
@@ -24,7 +24,14 @@ const limiter = rateLimit({
 })
 
 app.set('trust proxy', 1)
-app.use(helmet())
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+      "img-src": ["'self'", "data:", "https://images.unsplash.com"],
+    },
+  },
+}))
 app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173', credentials: true }))
 app.use(cookieParser())
 app.use(express.json({ limit: '10mb' }))
